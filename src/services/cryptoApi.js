87dedfbad76eb5a0d1ce022f5add27a506036f75
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-09-17 23:37:49 +0300

    add crypto details page

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -13,6 +13,9 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
   }),
 });
 
@@ -34,4 +37,4 @@ export const cryptoApi = createApi({
 //   },
 // };
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
