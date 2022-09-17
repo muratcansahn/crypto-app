31ede77417625a0dcf3b83be0c7316ca535fa379
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-09-17 19:14:39 +0300

    complete crytocurrencies component

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Spin, Input } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = () => {
-  return <div>Cryptocurrencies</div>;
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  ///rename data to cryptos
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  console.log(cryptosList?.data?.coins);
+  if (isFetching) return <Spin />;
+
+  return (
+    <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) =>
+              setCryptos(
+                cryptosList?.data?.coins.filter((coin) =>
+                  coin.name.toLowerCase().includes(e.target.value)
+                )
+              )
+            }
+          />
+        </div>
+      )}
+      <Row gutter={[12, 12]} className="crpto-card-container">
+        {cryptos?.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card">
+            <Link to={`/crypto/${currency.uuid}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Cryptocurrencies;
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,23 +1,19 @@
 import millify from "millify";
-import { Typography, Row, Col, Statistic } from "antd";
+import { Typography, Row, Col, Statistic, Spin } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from "../components";
 const { Title } = Typography;
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
-  const {
-    total,
-    totalExchanges,
-    totalMarketCap,
-    total24hVolume,
-    totalMarkets,
-  } = data?.data?.stats;
-  const globalStats = [
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
+  if (isFetching) return <Spin />;
+  const { total, totalExchanges, totalMarketCap, total24hVolume } = globalStats;
+  const stats = [
     { title: "Total Cryptocurrencies", value: total, suffix: "" },
     { title: "Total Exchanges", value: totalExchanges, suffix: "" },
     { title: "Total Market Cap", value: totalMarketCap, suffix: "$" },
     { title: "Total 24h Volume", value: total24hVolume, suffix: "$" },
-    { title: "Total Markets", value: totalMarkets, suffix: "" },
   ];
 
   return (
@@ -26,7 +22,7 @@ const Homepage = () => {
         Global Crypto Stats
       </Title>
       <Row>
-        {globalStats.map(({ title, value, suffix }) => (
+        {stats.map(({ title, value, suffix }) => (
           <Col span={12}>
             <Statistic title={title} value={millify(value)} suffix={suffix} />
           </Col>
@@ -40,7 +36,16 @@ const Homepage = () => {
           <Link to="/cryptocurrencies">Show More</Link>
         </Title>
       </div>
-      <div className="table-container"></div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
